c9159bf56a342e7de3dca4e359d82f4897de77c0
isabellabond <ibond713@gmail.com>
2019-09-10 09:49:20 -0400

    attempted fix

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var requestHandler = function(request, response) {
   }
   // send a 404 error
   else{
-    response.type('text/plain');
+    response.write('text/plain');
     response.status(404);
     response.write('Bad gateway error');
     response.end();
