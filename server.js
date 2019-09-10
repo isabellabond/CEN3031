8a70526f684313305dbae28fda8cf93b59ebc0e1
isabellabond <ibond713@gmail.com>
2019-09-10 08:41:58 -0400

    changes to server.js

--- server.js
+++ server.js
@@ -12,40 +12,46 @@ var requestHandler = function(request, response) {
   /*
     Your request handler should send listingData in the JSON format as a response if a GET request 
     is sent to the '/listings' path. Otherwise, it should send a 404 error. 
+  */
 
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-    
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   */
+  // check and see if GET request is sent to the '/listings' path
+  if(parsedUrl.pathname == '/listings'){
+    //send listingData as a response
+    response.write(listingData);
+    response.end();
+  }
+  // send a 404 error
+  else{
+    response.type('text/plain');
+    response.status(404);
+    response.write('404 error');
+    response.end();
+  }
+
 };
 
+// this is readinglistings.json
+// either will send what is in this file to the function as data or produce an error err
 fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
-
-    HINT: Check out this resource on fs.readFile
-    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
-
-    HINT: Read up on JSON parsing Node.js
-   */
-
-    //Check for errors
   
+  // check for errors
+  // use err to check
+  if(err){
+    console.log('There was an error reading the listings.json file');
+    return;
+  }
+  // save the sate in the listingData variable already defined
+  // if there is no error then save the data in listingData
+  else{
+    listingData = data;
+  }
 
-   //Save the sate in the listingData variable already defined
-  
+  // create the server
+  var server = http.createServer(requestHandler);
 
-  //Creates the server
-  
-  //Start the server
-
+  // start the server
+  server.listen(port, function() {
+  	console.log('Server is listening on http://127.0.0.1:' + port);
+  });
 
 });
